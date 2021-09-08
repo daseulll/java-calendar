a310b6c054960ba1f7cb61e23d3d7c2f1227704e
daseulll <nldaseul@gmail.com>
2021-09-09 00:21:09 +0900

    println으로 캘린더 모양 출력하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -2,6 +2,32 @@ package calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar!");
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("----------------------");
+		
+		String week = ""; 
+		for (int i = 1; i <= 29; i++) {
+			int reapeatCount;
+			int remainder = i % 7;
+			
+			if (remainder == 1) {
+				reapeatCount = 0;
+				if (i < 10) {
+					reapeatCount = 1;				
+				}
+			}
+			else {
+				reapeatCount = 1;
+				if (i < 10) {
+					reapeatCount = 2;				
+				}
+			}
+			
+			week = week + " ".repeat(reapeatCount) + Integer.toString(i);
+			if (remainder == 0) {				
+				System.out.println(week);
+				week = "";
+			}
+		}
 	}
 }
