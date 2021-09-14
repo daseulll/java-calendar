98c3c8d674cd2563bb726e7e3e3bd7207501721a
daseulll <nldaseul@gmail.com>
2021-09-15 07:26:26 +0900

    static하지 않게 메서드 호출하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Calendar {
 	private static final int[] LAST_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public static int lastDayOfMonth(int month) {
+	public int lastDayOfMonth(int month) {
 		return LAST_DAYS[month - 1];
 	}
 	
@@ -15,7 +15,8 @@ public class Calendar {
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, lastDayOfMonth(month));
+		Calendar cal = new Calendar();
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.lastDayOfMonth(month));
 		
 		scanner.close();
 	}
