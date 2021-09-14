44a7545c257a87c4713ab5d13135c5505f1d75db
daseulll <nldaseul@gmail.com>
2021-09-15 07:23:17 +0900

    [풀이] 함수활용하여 해당 달의 마지막날짜 구하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,16 +3,19 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] LAST_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static int lastDayOfMonth(int month) {
+		return LAST_DAYS[month - 1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("달을 입력하세요. ");
 		
-		int[] lastDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
 		Scanner scanner = new Scanner(System.in);
-		int inputValue = scanner.nextInt();
+		int month = scanner.nextInt();
 		
-		int lastDayOfMonth =  lastDays[inputValue + 1];
-		System.out.printf("%d월은 %d일까지 있습니다.", inputValue, lastDayOfMonth);
+		System.out.printf("%d월은 %d일까지 있습니다.", month, lastDayOfMonth(month));
 		
 		scanner.close();
 	}
