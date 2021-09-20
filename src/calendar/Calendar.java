15f7781b835dcda1c0831b2e3f197bb73466d3ec
daseulll <nldaseul@gmail.com>
2021-09-20 10:04:30 +0900

    윤년계산 적용하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,32 +4,45 @@ import java.util.Scanner;
 
 public class Calendar {
 	private static final int[] LAST_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_LAST_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getLastDayOfMonth(int month) {
+		return LAST_DAYS[month - 1];
+	}
+
+	public int getLastDayOfMonth2(int year, int month) {
+		if (year % 400 == 0) {
+			return LEAP_LAST_DAYS[month - 1];
+		} else if (year % 100 == 0) {
+			return LAST_DAYS[month - 1];
+		} else if (year % 4 == 0) {
+			return LEAP_LAST_DAYS[month - 1];
+		}
+
 		return LAST_DAYS[month - 1];
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		int month = 1;
-		
+
 		while (true) {
-			System.out.println("달을 입력하세요. ");
+			System.out.println("출력할 연도와 월을 입력하세요. ex) 2021 9");
 			System.out.print("$");
-			month = scanner.nextInt();
-			
-			if (month == -1) {
+			int year = scanner.nextInt();
+			int month = scanner.nextInt();
+
+			if (year == -1 || month == -1) {
 				break;
 			}
-			
+
 			else if (month > 12) {
 				System.out.println("1과 12 사이의 값만 입력가능합니다.");
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getLastDayOfMonth(month));
+			System.out.printf("%d년 %d월은 %d일까지 있습니다.\n", year, month, cal.getLastDayOfMonth2(year, month));
 		}
-		
+
 		System.out.println("Bye!");
 		scanner.close();
 	}
