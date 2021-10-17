fa1779dc1b62ee945824eb1f2ba05a66e3198f6c
daseulll <nldaseul@gmail.com>
2021-10-17 15:11:01 +0900

    Refactor FakeCalendar.java

--- src/calendar/FakeCalendar.java
+++ src/calendar/FakeCalendar.java
@@ -7,18 +7,42 @@ public class FakeCalendar {
 	public static int getDayIndex(int year, int month) {
 		// 그레고리력 1년 1월 1일은 월요일 (1)
 		int leapYearCount = (year / 4) - (year / 100) + (year / 400);
-		
-		int weekday = (year - leapYearCount) + (leapYearCount*2);
-		
+
+		int weekday = (year - leapYearCount) + (leapYearCount * 2);
+
 		for (int i = 1; i < month; i++) {
 			int lastDayOfMonth = new Calendar().getLastDayOfMonth(year, i);
 			int diff = lastDayOfMonth - 28;
 			weekday += diff;
 		}
-				
+
 		return weekday % 7;
 	}
-	
+
+	public static void printCalendar(int year, int month) {
+		System.out.printf("<< %d년 %d월 >>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("=====================");
+
+		int lastDayOfMonth = new Calendar().getLastDayOfMonth(year, month);
+		int dayIndex = getDayIndex(year, month);
+
+		if (dayIndex > 0) {
+			System.out.printf(("%" + Integer.toString(dayIndex * 3) + "s"), " ");
+		}
+
+		int currentDayIndex = dayIndex;
+		for (int i = 1; i <= lastDayOfMonth; i++) {
+			System.out.printf("%3d", i);
+			currentDayIndex += 1;
+
+			if (currentDayIndex == 7 || i == lastDayOfMonth) {
+				currentDayIndex = 0;
+				System.out.println();
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
@@ -27,41 +51,18 @@ public class FakeCalendar {
 			System.out.print("$");
 			int year = scanner.nextInt();
 			int month = scanner.nextInt();
-			
-			int dayIndex = getDayIndex(year, month);
-			
-			
+
 			if (year == -1 || month == -1) {
 				break;
-			}
-			else if (month < 1 || month > 12) {
+			} else if (month < 1 || month > 12) {
 				System.out.println("1과 12 사이의 값만 입력해주세요.");
 				continue;
 			}
 
-			int lastDayOfMonth = new Calendar().getLastDayOfMonth(year, month);
-
-			System.out.printf("<< %d년 %d월 >>\n", year, month);
-			System.out.println(" SU MO TU WE TH FR SA");
-			System.out.println("=====================");
-			
-			if (dayIndex > 0) {				
-				System.out.printf(("%"+ Integer.toString(dayIndex * 3) +"s"), " ");
-			}
-			
-			int currentDayIndex = dayIndex; 
-			for (int i = 1; i <= lastDayOfMonth; i++) {				
-				System.out.printf("%3d", i);				
-				currentDayIndex += 1;
-				
-				if (currentDayIndex == 7 || i == lastDayOfMonth) {
-					currentDayIndex = 0;
-					System.out.println();
-				}
-			}
+			printCalendar(year, month);
 		}
 		scanner.close();
-		
+
 		System.out.println("Bye!");
 	}
 }
